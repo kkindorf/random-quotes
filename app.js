d041a4c55f9ee1b68926700b435933c3b5513457
kkindorf <kevin.kindorf@gmail.com>
2016-11-17 22:43:13 -0500

    updated quote generator json

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         $('#changeMessage').css('background-color', color);
         $('button').css('color', 'white');
         $('.fa-twitter').css('color', color);
-        $.getJSON("http://api.icndb.com/jokes/random?exclude=[explicit]?callback=", function(json) {
+        $.getJSON("https://api.icndb.com/jokes/random?exclude=[explicit]?callback=", function(json) {
             var quote = json.value["joke"];
             var quoteLength = quote.length;
                 $(".quote").html("<h3>" + quote + "</h3>");
